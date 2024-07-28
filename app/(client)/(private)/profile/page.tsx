40f88034cc05ca1a75a94424a115ccf46950282a
Khalidprithy <khalidprithy@gmail.com>
2024-07-29 00:11:56 +0600

    footer updated

--- app/(client)/(private)/profile/page.tsx
+++ app/(client)/(private)/profile/page.tsx
@@ -5,8 +5,8 @@ export default async function Page() {
 
   return (
     <div className="flex items-center justify-center">
-      <div className="rounded-lg border bg-white p-8 text-center shadow">
-        <h1 className="text-lg font-semibold text-gray-800">
+      <div className="rounded-lg border p-8 text-center shadow">
+        <h1 className="text-lg font-semibold">
           Welcome, {session?.user?.email}
         </h1>
       </div>
--- components/layout/client/footer.tsx
+++ components/layout/client/footer.tsx
@@ -1,7 +1,15 @@
+import { Icons } from '@/components/icons';
+
 export default function footer() {
   return (
     <footer className="border-t bg-background/95 p-2 text-center backdrop-blur">
-      <small>All rights reserved by @me.</small>
+      <div className="flex items-center justify-center gap-2">
+        <small>All rights reserved by &copy; 2024 dev.kbin</small>
+
+        <a href="https://github.com/Khalidprithy" target="_blank">
+          <Icons.gitHub className="h-4 w-4" />
+        </a>
+      </div>
     </footer>
   );
 }
